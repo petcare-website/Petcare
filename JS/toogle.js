ff8022bd99c0658baba2d6fcba29443eb1240a91
S Noorunnisa <shaiknoorunnisa45@gmail.com>
2024-11-19 11:35:34 +0530

    Initial files added

---
+++ JS/toogle.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var x = document.getElementById("topnav");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
